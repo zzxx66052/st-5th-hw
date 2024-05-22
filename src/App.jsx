885bc10eaa74d598a70d80f42bb5b30805397dba
Jecheon Ryu <rjc1704@gmail.com>
2024-05-22 12:03:34 +0900

    ex1 라우터 설정 문제

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  /**
+   * TODO: 아래와 같은 구조로 컴포넌트를 만들고 Browser Router 설정하세요. UI는 신경쓰지 않습니다. 별도의 Router 컴포넌트 생성 여부는 자유입니다.
+   * Home 컴포넌트는 <Link> 컴포넌트를 사용해서 Detail 컴포넌트로 이동하도록 하세요.
+   * Detail 컴포넌트를 path parameter 로 id 를 받도록 하세요.
+   * Detail 컴포넌트는 path parameter 로 받은 id 를 콘솔로그로 찍으세요.
+   * 
+   * src/
+    |-- pages/
+    |   |-- Home.jsx
+    |   |-- Detail.jsx
+    |-- App.jsx
+   */
+
+  return <></>;
+}
+
+export default App;
